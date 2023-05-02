33bbb468f64defe73c8401622d6d38c852fe91fe
dolezj42 <dolezj42@fit.cvut.cz>
2023-05-02 20:19:06 +0300

    make topBar component

---
+++ src/components/topBar.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import styles from "./Home/home.module.css";
+import Login from "./Login/Login";
+import {Link} from "react-router-dom";
+
+
+function TopBar(){
+  return (
+    <div className="topBar">
+    <img
+    //  src="/playground_assets/rectangle7149-hc5o-200h.png"
+      alt=""
+      className={styles['rectangle7']}
+    />
+      <Link to={"/"}><link/>HOME
+  <span className={styles['text']}>
+        <span>TAKE a BEER</span>
+        </span>
+      </Link>
+  <span className={styles['text02']}>
+          <span>EN</span>
+        </span>
+      <Link to="/login"><link/>
+  <span className={styles['text04']}>
+          <span>account</span>
+        </span>
+      </Link>
+      <img
+        src={require("../Content/ecM7apobi.png")}
+        alt="ecM7apobi11100"
+        className={styles['ec-m7apobi1']}
+      />
+    </div>
+  );
+}
+export default TopBar;
