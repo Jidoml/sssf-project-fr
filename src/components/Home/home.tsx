6904ce2739902e6682689f9a38013b627e144c1f
dolezj42 <dolezj42@fit.cvut.cz>
2023-05-02 20:18:53 +0300

    make home page

---
+++ src/components/Home/home.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+import styles from './home.module.css'
+import TopBar from "../topBar";
+
+function Home() {
+  return (
+    <div className={styles['container']}>
+      <div className={styles['home']}>
+        <img
+          src={require("../../Content/beer-background-ZC2QTEK-scaled.jpg")}
+          alt="beerbackgroundZC2QTEKscaled1148"
+          className={styles['beerbackground-zc2qt-kscaled1']}
+        />
+        <TopBar/>
+        <span className={styles['text06']}>
+          <span>Do you like taste of beer? You are on a right place.</span>
+        </span>
+        <button
+          className={styles['rectangle8']}
+        />
+        <button
+          className={styles['rectangle9']}
+        />
+        <button
+          className={styles['rectangle10']}
+        />
+        <img
+          src={require("../../Content/img_405324.png")}
+          alt="img4053241158"
+          className={styles['img4053241']}
+        />
+        <span className={styles['text08']}>
+          <span>About us</span>
+        </span>
+        <img
+          src={require("../../Content/brewery-icon-13.jpg.png")}
+          alt="breweryicon131160"
+          className={styles['breweryicon131']}
+        />
+        <span className={styles['text10']}>
+          <span>Product offer</span>
+        </span>
+        <img
+          src={require("../../Content/3067706.png")}
+          alt="IMAGE30677061162"
+          className={styles['image30677061']}
+        />
+        <span className={styles['text12']}>
+          <span>Why craft beer?</span>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Home
