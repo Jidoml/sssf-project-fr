0685f5992009716cffac8e5dc959903f417bff7c
dolezj42 <dolezj42@fit.cvut.cz>
2023-05-03 23:48:16 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,41 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/home";
 import Login from "./components/Login/Login";
+import DrinkList from "./components/DrinkList/DrinkList";
+import BarrelList from "./components/BarrelList/BarrelList";
+import ShoppingCart from "./components/ShoppingBag/shoppingCart";
+import { setToken, setId} from './Reducer/authSlice';
+import UserDetail from "./components/UserInformation/UserDetail";
+import {useAppDispatch} from "./Reducer/hooks";
+import Register from "./components/Register/Register";
+import styles from "./components/Home/home.module.css";
+import TopBar from "./components/topBar";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
+  const dispatch = useAppDispatch();
 
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+  const handleLogin = (token: string, userId: string) => {
+    dispatch(setToken(token));
+    dispatch(setId(userId));
+  };
+
+  return (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+
+      <Route path="/login" element={<Login  onLogin={handleLogin}/>} />
+
+      <Route path="/register" element={<Register />} />
+
+      <Route path="/user" element={<UserDetail />} />
+
+      <Route path="/productOffer" element={<DrinkList />} />
+      <Route path="/productOffer/:drinkId" element={<BarrelList />} />
+
+      <Route path="/shoppingCart" element={<ShoppingCart />} />
+    </Routes>
+  </Router>
   );
 }
 
