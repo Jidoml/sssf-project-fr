ecf41b2d8afc5bcd1ec27f0145d0a545a04b8ba9
dolezj42 <dolezj42@fit.cvut.cz>
2023-05-05 12:14:14 +0300

    better design

--- src/components/UserInformation/UserDetail.tsx
+++ src/components/UserInformation/UserDetail.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {selectToken, selectUserId} from "../../Reducer/authSlice";
 import {useAppSelector} from "../../Reducer/hooks";
+import styles from "../CheckOut/checkOut.module.css";
+import {Link} from "react-router-dom";
+import LoanTable from "./loanTable";
 
 
 function UserDetail() {
@@ -13,6 +16,14 @@ function UserDetail() {
       <h1>User Detail</h1>
       <p>{userID}</p>
       <p>{token}</p>
+      <div className={styles['baseBox']}>
+        <LoanTable />
+      </div>
+      <Link to={"/"}>
+        <button className={styles['button1']}>
+          <span className={styles['simpleText']}>Login</span>
+        </button>
+      </Link>
     </div>
   );
 }
