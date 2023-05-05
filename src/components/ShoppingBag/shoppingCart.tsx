848cf17befa2c41cf78a921b766c67030a4f1e9c
dolezj42 <dolezj42@fit.cvut.cz>
2023-05-05 12:13:51 +0300

    new page shopping cart

--- src/components/ShoppingBag/shoppingCart.tsx
+++ src/components/ShoppingBag/shoppingCart.tsx
@@ -5,26 +5,44 @@ import ShoppingCartItem from "./shoppingCartItem";
 import styles from "./shoppingCart.module.css"
 import TopBar from "../topBar";
 import {List} from "@mui/material";
+import {Link} from "react-router-dom";
 
 function ShoppingCart() {
   let barrelIds = useAppSelector(selectBarrelIds);
 
   return (
     <div>
-        <img
-          src={require("../../Content/beer-background-ZC2QTEK-scaled.jpg")}
-          alt="beerbackgroundZC2QTEKscaled1148"
-          className={styles['beerbackground-zc2qt-kscaled1']}
-        />
-        <TopBar/>
-        <List className={styles['containerList']}>
-          {
-            barrelIds ?
-              barrelIds.map((barrelId) => (
-                <ShoppingCartItem barrelId={barrelId}/>
-              )) : <p>No drinks selected</p>
-          }
-        </List>
+      <img
+        src={require("../../Content/beer-background-ZC2QTEK-scaled.jpg")}
+        alt="beerbackgroundZC2QTEKscaled1148"
+        className={styles['beerbackground-zc2qt-kscaled1']}
+      />
+      <TopBar/>
+      <p className={styles['mainText']}>Review your bag</p>
+      <div className={styles['container']}>
+        {
+          barrelIds ?
+            <div>
+              <List className={styles['containerList']}>
+                {
+                  barrelIds.map((barrelId) => (
+                    <ShoppingCartItem key={barrelId} barrelId={barrelId}/>
+                  ))}
+              </List>
+              <Link to={"/productOffer"} className={styles['bottomField']}>
+                <button className={styles['button1']}>
+                  <span className={styles['simpleText']}>Continue shopping</span>
+                </button>
+              </Link>
+              <Link to={"/checkOut"}>
+                <button className={styles['button2']}>
+                  <span className={styles['simpleText']}>Check out</span>
+                </button>
+              </Link>
+            </div>
+            : <p className={styles['mainText']}>No drinks selected</p>
+        }
+      </div>
     </div>
   );
 }
